178dd7d6767f4ab06c735d4cd5c3c55b51ddf0d0
andrew arkwright <andrew.arkwright@hotmail.com>
2022-11-02 21:57:48 -0400

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+import Button from "./Button"
+
+const Header = ({ title, onAdd, showAdd }) => {
+
+  return (
+    <header className = "header">
+        <h1>{title}</h1>
+        <Button onClick = {onAdd} text={showAdd ? "Close" : "Add"}/>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: "Todo List"
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header
